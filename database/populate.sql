28ea96efcbe20c01532c1cb77d6268ff9c620b59
Daniel Rodrigues <up202006562@up.pt>
2022-10-23 14:44:58 +0100

    Add data - colors

---
+++ database/populate.sql
@@ -0,0 +1,15 @@
+--COLOR
+insert into color (id, name) values (1, 'Fuscia');
+insert into color (id, name) values (2, 'Crimson');
+insert into color (id, name) values (3, 'Turquoise');
+insert into color (id, name) values (4, 'Mauv');
+insert into color (id, name) values (5, 'Maroon');
+insert into color (id, name) values (6, 'Blue');
+insert into color (id, name) values (7, 'Pink');
+insert into color (id, name) values (8, 'Teal');
+insert into color (id, name) values (9, 'Orange');
+insert into color (id, name) values (10, 'Red');
+insert into color (id, name) values (11, 'Khaki');
+insert into color (id, name) values (12, 'Aquamarine');
+insert into color (id, name) values (13, 'Yellow');
+insert into color (id, name) values (14, 'Purple');
