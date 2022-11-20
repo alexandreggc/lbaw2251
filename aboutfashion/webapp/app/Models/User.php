175374858558cada2cc2841c71d25922e6630dc1
diogofonte <diogo.fonte2000@gmail.com>
2022-11-20 13:22:12 +0000

    correction in user model

--- aboutfashion/webapp/app/Models/User.php
+++ aboutfashion/webapp/app/Models/User.php
@@ -77,7 +77,7 @@ class User extends Authenticatable
     }
 
     public function reviews(){
-        return $this->hasMany('App\Model\Review', 'id_user');
+        return $this->hasMany('App\Models\Review', 'id_user');
     }
     
 }
