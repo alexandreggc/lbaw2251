6922ac1bd33e0ec8a6d3600017ad5b838d284913
Daniel Rodrigues <up202006562@up.pt>
2022-11-21 22:43:44 +0000

    implement shopping cart backend

--- aboutfashion/webapp/app/Http/Controllers/PageController.php
+++ aboutfashion/webapp/app/Http/Controllers/PageController.php
@@ -12,4 +12,12 @@ class PageController extends Controller
     public function homePageAdmin(){
         return view('pages.admin.home');
     }
+
+    public function homePage(){
+        return view('pages.home');
+    }
+
+    public function homePageAdmin(){
+        return view('pages.admin.home');
+    }
 }
