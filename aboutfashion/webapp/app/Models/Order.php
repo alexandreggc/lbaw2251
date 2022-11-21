c56d3887abf3cb07d8870ceec8475eeea0b8c085
diogofonte <diogo.fonte2000@gmail.com>
2022-11-21 00:18:16 +0000

    fix order model name

--- aboutfashion/webapp/app/Models/Order.php
+++ aboutfashion/webapp/app/Models/Order.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Oder extends Model
+class Order extends Model
 {
     public $timestamps = false;
     protected $table = 'model';
