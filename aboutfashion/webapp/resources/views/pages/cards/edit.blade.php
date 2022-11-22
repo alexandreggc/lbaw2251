64b9729682604ab6f8269d25e2045ce21848262c
Alexandre Guimaraes Gomes Correia <up202007042@fe.up.pt>
2022-11-22 19:33:44 +0000

    Added card editor

---
+++ aboutfashion/webapp/resources/views/pages/cards/edit.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div id="edit_form">
+    <legend>Edit Card</legend>
+    {{--definir outra routa--}}
+    <form method="POST" action="{{ route('userRegister') }}">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label for="nick_name" class="form-label mt-4">Nick Name</label>
+            <input type="text" class="form-control" id="nick_name" placeholder="Nick Name" name="nick_name" required>
+        </div>
+        <div class="form-group">
+            <label for="name" class="form-label mt-4">Name</label>
+            <input type="text" class="form-control" id="name" placeholder="Name" name="name" required>
+        </div>
+        <div class="form-group">
+            <label for="number" class="form-label mt-4">Number</label>
+            <input type="text" class="form-control" id="number" placeholder="Number" name="number" required>
+        </div>
+        <div class="form-group">
+            <label for="month" class="form-label mt-4">Month</label>
+            <input type="number" class="form-control" id="month" placeholder="Month" name="month" required>
+        </div>
+        <div class="form-group">
+            <div class=" me-auto"></div>
+            <label for="month_year" class="form-label mt-4">Month/Year:</label>
+            <input type="month" id="month_year" class="form-control" name="month_year" min="2022-11" value="2022-11">
+        </div>
+        <div class="form-group">
+            <label for="code" class="form-label mt-4">Code</label>
+            <input type="number" class="form-control" id="code" placeholder="Code" name="code" required>
+        </div>
+        <div class="modal-footer">
+            <span class="error-text me-auto" style="color:red"> </span>
+            <button type="submit" class="btn btn-primary reg">Save</button>
+        </div>
+    </form>
+    
+</div>
+
+@endsection
--- aboutfashion/webapp/routes/web.php
+++ aboutfashion/webapp/routes/web.php
@@ -17,6 +17,9 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::get('/order/{id}', 'OrderController@show')->name('orderDetails');
 
+//Cards
+Route::get('/cards/{id}/edit', 'CardController@edit')->name('cardEditForm');
+
 //Admin
 Route::get('/admin-panel/login', 'Auth\LoginController@showLoginForm')->name('adminLoginForm');
 Route::post('/admin-panel/login', 'Auth\LoginController@adminLogin')->name('adminLogin');
