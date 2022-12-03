270aaca36f8d4f9c62d93cfad361c703ed94e7c4
Daniel Rodrigues <up202006562@up.pt>
2022-12-03 16:17:07 +0000

    .

--- aboutfashion/webapp/app/Http/Controllers/CategoryController.php
+++ aboutfashion/webapp/app/Http/Controllers/CategoryController.php
@@ -7,23 +7,12 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller{
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @param  Request
-     * @return Response
-     */
+
     public function create(Request $request){
         $category = store($request);
         return view('categories.create', ['category' => $category]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  Request
-     * @return Response
-     */
     public function store(Request $request){
         $category = new Category();
         $this->authorize('store', $category);
@@ -35,24 +24,14 @@ class CategoryController extends Controller{
         return $category;
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  Request
-     * @return Response
-     */
+
     public function edit($request){
         //implementar esta policy update no CategoryPolicy
         $category = update($request);
         return view('categories.edit', ['category' => $category]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  Request
-     * @return Response
-     */
+
     public function update(Request $request){
         $category = Category::find($request->input('id'));
         $this->authorize('update', $category);
@@ -72,38 +51,5 @@ class CategoryController extends Controller{
         return $category;
     }
 
-    public function getAllSuperCategories(Request $request){
-        $categories = array();
-        $superCategory = Category::findOrFail($id)->superCategory()->get();
-        while(count($superCategory) != 0){
-            array_push($categories, $superCategory);
-            $superCategory = Category::findOrFail($superCategory[0]['id'])->superCategory()->get();
-        }
-        
-        return $categories;
-    }
-
-    public function getAllSubCategories($id){
-        $categories = array();
-        $unexploredCategories = array();
-
-        do{
-            $categoriesDB = Category::findOrFail($id)->subCategories()->get();
-            if(count($categoriesDB) != 0){
-                foreach($categoriesDB as $category){
-                    array_push($unexploredCategories,$category['id']);
-                    array_push($categories, $category);
-                }   
-            }
-            
-            if(count($unexploredCategories) == 0){
-                break;
-            }else{
-                $id = $unexploredCategories[0];
-                array_splice($unexploredCategories, 0, 1);
-            }
-            
-        }while(true);    
-        return $categories;
-    }
+    
 }
