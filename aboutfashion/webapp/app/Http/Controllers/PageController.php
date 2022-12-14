c0452b1dc869ff8b784e92ba8bcb31d426d13a1b
diogofonte <diogo.fonte2000@gmail.com>
2022-12-14 23:19:09 +0000

    fixed routes and page controller

--- aboutfashion/webapp/app/Http/Controllers/PageController.php
+++ aboutfashion/webapp/app/Http/Controllers/PageController.php
@@ -10,38 +10,6 @@ use App\Models\Report;
 
 class PageController extends Controller{
 
-    /*public function homePageAdmin(){
-        //por default /admin-panel aparecerá a página de users
-        // ver se é melhor fazer um redirect para /admin-panel/users e criar uma nova página de admin home para n ser direto
-        $users = User::all();
-        return view('pages.admin.home', ['users'=>$users]);
-    }
-
-    public function productsPageAdmin(){
-        $products = Product::all();
-        return view('pages.admin.products', ['products'=>$products]);
-    }
-
-    public function promotionsPageAdmin(){
-        $promotions = Promotion::all();
-        return view('pages.admin.promotions', ['promotions'=>$promotions]);
-    }
-
-    public function ordersPageAdmin(){
-        $orders = Order::all();
-        return view('pages.admin.orders', ['orders'=>$orders]);
-    }
-
-    public function reviewsPageAdmin(){
-        $reviews = Review::all();
-        return view('pages.admin.reviews', ['reviews'=>$reviews]);
-    }
-
-    public function reportsPageAdmin(){
-        $reports = Report::all();
-        return view('pages.admin.reports', ['reports'=>$reports]);
-    }*/
-
     public function homePage(){
         $promotions = Promotion::all();
         return view('pages.home',['promotions'=>$promotions]);
