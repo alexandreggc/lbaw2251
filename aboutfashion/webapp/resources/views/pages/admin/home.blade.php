c3caa1880164fc6d8c07932a719d69d4f6e92433
diogofonte <diogo.fonte2000@gmail.com>
2022-12-15 00:14:46 +0000

    accordion instead of a table in users page

--- aboutfashion/webapp/resources/views/pages/admin/home.blade.php
+++ aboutfashion/webapp/resources/views/pages/admin/home.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <h2 class="p-3">Hi Admin!</h2>
         </div>
-        <div class="row mb-5">
+        <div class="row mb-3">
             <ul class="nav nav-pills m-3">
                 <li class="nav-item">
                     <a class="nav-link active" href="/admin-panel">Users</a>
@@ -26,6 +26,8 @@
                     <a class="nav-link" href="/admin-panel/reports">Reports</a>
                 </li>
             </ul>
+        </div>
+        <div class="row">
             <div class="col-6"></div>
             <div class="col-6 mb-3">
                 <form class="d-flex">
@@ -33,6 +35,57 @@
                     <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
+        </div>
+        <div class="row">
+            <div class="accordion" id="accordion">
+                @foreach($users as $user)
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="heading{{$user->id}}">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" 
+                                    data-bs-target="#collapse{{$user->id}}" aria-expanded="true" 
+                                    aria-controls="collapse{{$user->id}}">
+                                @if ($user->blocked == 1)
+                                    <div class="col">
+                                        {{$user['first_name'] . ' ' . $user['last_name']}}
+                                        <span class="badge bg-danger ms-3">Blocked</span>
+                                    </div>
+                                @else
+                                    <div class="col">
+                                        {{$user['first_name'] . ' ' . $user['last_name']}}
+                                    </div>
+                                @endif
+                                <div class="col-1">
+                                    @if ($user->blocked == 1)
+                                    <a href="#">
+                                        <i class="fa-regular fa-lock-open" data-bs-container="body" data-bs-toggle="popover" 
+                                           data-bs-placement="left" data-bs-original-title="Popover Title">
+                                        </i>
+                                    </a>
+                                    @else
+                                    <a href="#">
+                                        <i class="fa-regular fa-user-lock"></i>
+                                    </a>
+                                    @endif
+                                    <a href="#">
+                                        <i class="fa-regular fa-trash ps-5"></i>
+                                    </a>
+                                </div>
+                            </button>
+                        </h2>
+                        <div id="collapse{{$user->id}}" class="accordion-collapse collapse" 
+                            aria-labelledby="heading{{$user->id}}" data-bs-parent="#accordion">
+                            <div class="accordion-body">
+                                <strong>E-mail:</strong> {{$user['email']}}
+                                <br>
+                                <strong>Birth date:</strong> {{ isset($user['birth_date']) ? substr($user['birth_date'], 0, 10) : 'Not Defined'}}
+                                <br>
+                                <strong>Gender:</strong> {{ isset($user['gender']) ? $user['gender'] : 'Not Defined' }}
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            <!--
             <div id="usersList">
                 <table class="table">
                     <thead>
@@ -88,6 +141,7 @@
                     </tbody>
                 </table>
             </div>
+            -->
         </div>
     </div>
 @endsection
