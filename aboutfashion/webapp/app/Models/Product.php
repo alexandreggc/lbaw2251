c13923f23060d14c4da28e2ab4519a8e05a9fca1
anasofia2407 <ana.sgvs.costa@gmail.com>
2022-12-28 15:56:32 +0000

    full text search

--- aboutfashion/webapp/app/Models/Product.php
+++ aboutfashion/webapp/app/Models/Product.php
@@ -65,6 +65,6 @@ class Product extends Model{
         if (!$search) {
             return $query;
         }
-        return $query->whereRaw('tsvectors @@ plainto_tsquery(\'english\', ?)', [$search])->orderByRaw('ts_rank(tsvectors, to_tsquery(\'english\', ?)) DESC', [$search]);
+        return $query->whereRaw('tsvectors @@ plainto_tsquery(\'english\', ?)', [$search])->orderByRaw('ts_rank(tsvectors, plainto_tsquery(\'english\', ?)) DESC', [$search]);
     }
 }
