c8ec60cbd18da4d547d8f6613fd9be9c7983ad90
diogofonte <diogo.fonte2000@gmail.com>
2022-12-29 18:04:22 +0000

    new layout for admin panel

--- aboutfashion/webapp/resources/views/auth/login.blade.php
+++ aboutfashion/webapp/resources/views/auth/login.blade.php
@@ -19,16 +19,13 @@
   </head>
   <body>
     <main>
-      
       <section class="vh-100" style="background-color: #ffffff;">
         <div class="container py-5 h-100">
           <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-12 col-md-8 col-lg-6 col-xl-5">
               <div class="card shadow-2-strong" style="border-radius: 1rem;">
                 <div class="card-body p-5 text-center" >
-
                   <h3 class="mb-5 ">Sign in - Admin Panel</h3>
-
                   <form method="POST" action="{{ route('adminLogin') }}">
                     {{ csrf_field() }}
                     <div class="form-group text-start">
@@ -49,36 +46,28 @@
                         </span>
                       @endif
                     </div>
-
                     <div class="form-check mt-4 text-start">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault_login_Admin" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                        <label class="form-check-label" for="flexCheckDefault_login_Admin">
-                            Remember Me
-                        </label>
-                      </div>
-
-                      <div class="form-check mt-4 ">
-                      <a class="button button-outline  mt-4" href="{{ route('home') }}">Go back to user's homepage</a>
-                      </div>
-                    
+                      <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault_login_Admin" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                      <label class="form-check-label" for="flexCheckDefault_login_Admin">
+                          Remember Me
+                      </label>
+                    </div>
+                    <div class="form-check mt-4">
+                      <a class="button button-outline  mt-4" href="{{ route('home') }}">
+                        Go back to user's homepage
+                      </a>
+                    </div>
                     <hr class="my-4">
-                    <button type="submit" class="btn btn-primary" >
-                        Login
+                    <button type="submit" class="btn btn-primary">
+                      Login
                     </button>
-                    
-                    
-                </form>
-
-                  
-
+                  </form>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </section>
-      
-      
     </main>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
