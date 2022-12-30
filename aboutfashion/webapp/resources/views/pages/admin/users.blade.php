9d3d46e23c86027debaedd729c6955782edbd3b5
Daniel Rodrigues <up202006562@up.pt>
2022-12-30 16:56:09 +0000

    fix merge conflict

--- aboutfashion/webapp/resources/views/pages/admin/users.blade.php
+++ aboutfashion/webapp/resources/views/pages/admin/users.blade.php
@@ -8,27 +8,27 @@
             <h2 class="p-3">Users</h2>
         </div>
         <!--<div class="row mb-3">
-                <ul class="nav nav-pills m-3">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/admin-panel/users">Users</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/admin-panel/products">Products</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/admin-panel/promotions">Promotions</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/admin-panel/orders">Orders</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/admin-panel/reviews">Reviews</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/admin-panel/reports">Reports</a>
-                    </li>
-                </ul>
-            </div>-->
+                    <ul class="nav nav-pills m-3">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="/admin-panel/users">Users</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/admin-panel/products">Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/admin-panel/promotions">Promotions</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/admin-panel/orders">Orders</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/admin-panel/reviews">Reviews</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/admin-panel/reports">Reports</a>
+                        </li>
+                    </ul>
+                </div>-->
         <div class="row">
             <div class="col-6"></div>
             <div class="col-6 pb-3">
@@ -41,7 +41,7 @@
         <div class="row">
             <div class="accordion" id="accordion">
                 @foreach ($users as $user)
-                    <div class="accordion-item" id="accordion-item-{{$user->id}}">
+                    <div class="accordion-item" id="accordion-item-{{ $user->id }}">
                         <h2 class="accordion-header" id="heading-{{ $user->id }}">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#collapse-{{ $user->id }}" aria-expanded="true"
@@ -49,56 +49,61 @@
                                 <div class="col-1 pe-3">
                                     <img src="{{ $user->photo['file'] }}" alt="user photo" class="img-fluid">
                                 </div>
-                            @else
-                                <div class="col">
-                                    {{ $user['first_name'] . ' ' . $user['last_name'] }}
-                                </div>
-                            @endif
-                        </button>
-                    </h2>
-                    <div id="collapse-{{ $user->id }}" class="accordion-collapse collapse"
-                        aria-labelledby="heading-{{ $user->id }}" data-bs-parent="#accordion">
-                        <div class="accordion-body">
-                            <div class="row">
-                                <div class="col">
-                                    <strong>E-mail:</strong>
-                                    {{ $user['email'] }}
-                                    <br>
-                                    <strong>Birth date:</strong>
-                                    {{ isset($user['birth_date']) ? substr($user['birth_date'], 0, 10) : 'Not Defined' }}
-                                    <br>
-                                    <strong>Gender:</strong>
-                                    @if (is_null($user['gender']))
-                                        Not Defined
-                                    @elseif ($user['gender'] == 'M')
-                                        Male
-                                    @elseif ($user['gender'] == 'F')
-                                        Female
-                                    @else
-                                        Other
-                                    @endif
-                                </div>
-                                <div class="col-1">
-                                    <div class="row">
-                                        <a type="submit" class="btn btn-secondary btn-sm mb-3" 
+                                @if ($user->blocked == 1)
+                                    <div class="col">
+                                        {{ $user['first_name'] . ' ' . $user['last_name'] }}
+                                        <span class="badge bg-danger ms-3">Blocked</span>
+                                    </div>
+                                @else
+                                    <div class="col">
+                                        {{ $user['first_name'] . ' ' . $user['last_name'] }}
+                                    </div>
+                                @endif
+                            </button>
+                        </h2>
+                        <div id="collapse-{{ $user->id }}" class="accordion-collapse collapse"
+                            aria-labelledby="heading-{{ $user->id }}" data-bs-parent="#accordion">
+                            <div class="accordion-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <strong>E-mail:</strong> {{ $user['email'] }}
+                                        <br>
+                                        <strong>Birth date:</strong>
+                                        {{ isset($user['birth_date']) ? substr($user['birth_date'], 0, 10) : 'Not Defined' }}
+                                        <br>
+                                        <strong>Gender:</strong>
+                                        @if (is_null($user['gender']))
+                                            Not Defined
+                                        @elseif ($user['gender'] == 'M')
+                                            Male
+                                        @elseif ($user['gender'] == 'F')
+                                            Female
+                                        @else
+                                            Other
+                                        @endif
+                                    </div>
+                                    <div class="col-1">
+                                        <div class="row">
+                                            <a type="submit" class="btn btn-secondary btn-sm mb-3"
                                                 href="{{ route('userPurchaseHistoryAdminPanel', ['id' => $user->id]) }}">
                                                 <i class="fa-solid fa-bag-shopping"></i>
                                             </a>
                                         </div>
-                                        @if(Auth::guard('admin')->user()->role == 'Technician')
-                                        @if ($user['blocked'] == 0)
+                                        @if (Auth::guard('admin')->user()->role == 'Technician')
+                                            @if ($user['blocked'] == 0)
+                                                <div class="row">
+                                                    <button class="btn btn-warning btn-sm mb-3 fa-solid fa-lock"></button>
+                                                </div>
+                                            @else
+                                                <div class="row">
+                                                    <button class="btn btn-warning btn-sm mb-3 fa-solid fa-unlock"></button>
+                                                </div>
+                                            @endif
                                             <div class="row">
-                                                <button class="btn btn-warning btn-sm mb-3 fa-solid fa-lock"></button>
+                                                <button
+                                                    class="fa-solid fa-user-xmark btn btn-danger btn-sm pe-1 delete-user"
+                                                    id={{ $user->id }}></button>
                                             </div>
-                                        @else
-                                            <div class="row">
-                                                <button class="btn btn-warning btn-sm mb-3 fa-solid fa-unlock"></button>
-                                            </div>
-                                        @endif
-                                        <div class="row">
-                                            <button class="fa-solid fa-user-xmark btn btn-danger btn-sm pe-1 delete-user"
-                                                id={{ $user->id }}></button>
-                                        </div>
                                         @else
                                         @endif
                                     </div>
@@ -106,10 +111,9 @@
                             </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
-</div>
-{{ $users->links() }}
+    {{ $users->links() }}
 @endsection
