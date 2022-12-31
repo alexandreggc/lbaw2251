c3caabe3390107c9091e41b080e1c42f5eec7024
diogofonte <diogo.fonte2000@gmail.com>
2022-12-31 18:45:39 +0000

    add product page in admin panel

--- aboutfashion/webapp/resources/views/pages/admin/addProduct.blade.php
+++ aboutfashion/webapp/resources/views/pages/admin/addProduct.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <h2 class="p-3">Products</h2>
+            <h2 class="p-3 pb-5">Products</h2>
         </div>
-        <div class="row pb-3">
+        <div class="row">
             <div class="col-1">
                 <a href="/admin-panel/products">
                     <i class="fa-regular fa-arrow-left fa-2x"></i>
@@ -15,38 +15,40 @@
                 <h3>Add New Product</h3>
             </div>
         </div>
-        <div class="row pt-3">
-            
-            <form method="POST" action="{{ route('storeProduct') }}">
-                @csrf
-                @method('put')
-                <div class="form-group">
-                    <label for="productSelect" class="form-label mt-4"></label>
-                    <select class="form-select" id="categorySelect" name="id_category" onchange="showCategory()"> <!-- ATENÇÃO AO ONCHANGE -->
-                        <option>Select a category </option>
-                        @foreach ($categories as $category)
-                            <option value="{{$category['id']}}">{{$category['name']}}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="name" class="form-label mt-4">Name</label>
-                    <input type="text" class="form-control" id="product_name" value="{{$product->name}}" name="name">
-                </div>
-                <div class="form-group">
-                    <label for="description" class="form-label mt-4">Description</label>
-                    <input type="text" class="form-control" id="product_description" value="{{$product->description}}" name="description">
-                </div>
-                <div class="form-group">
-                    <label for="price" class="form-label mt-4">Price</label>
-                    <input type="number" class="form-control" id="product_price" value="{{$product->price}}" name="price">
-                </div>
-                <div class="modal-footer">
-                    <span class="error-text me-auto" style="color:red"> </span>
-                    <button type="submit" class="btn btn-primary reg">Save</button>
-                </div>
-            </form>
-
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col">
+                <form method="POST" action="{{ route('storeProduct') }}">
+                    @csrf
+                    @method('put')
+                    <div class="form-group">
+                        <label for="productSelect" class="form-label"></label>
+                        <select class="form-select" id="categorySelect" name="id_category" onchange="showCategory()"> <!-- ATENÇÃO AO ONCHANGE -->
+                            <option>Select a category </option>
+                            @foreach ($categories as $category)
+                                <option value="{{$category['id']}}">{{$category['name']}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="name" class="form-label mt-4">Name</label>
+                        <input type="text" class="form-control" id="product_name" name="name"> <!-- colocar value -->
+                    </div>
+                    <div class="form-group">
+                        <label for="description" class="form-label mt-4">Description</label>
+                        <input type="text" class="form-control" id="product_description" name="description"> <!-- colocar value -->
+                    </div>
+                    <div class="form-group">
+                        <label for="price" class="form-label mt-4">Price</label>
+                        <input type="number" class="form-control" id="product_price" name="price"> <!-- colocar value -->
+                    </div>
+                    <div class="modal-footer p-5 pe-0">
+                        <span class="error-text me-auto" style="color:red"> </span>
+                        <button type="submit" class="btn btn-primary reg btn-lg">Save</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-1"></div>
         </div>
     </div>
 @endsection
