9ff6cbacb02c011813a0c3c6f4b7832e3b2f0e75
diogofonte <diogo.fonte2000@gmail.com>
2023-01-02 20:34:10 +0000

    edit promo finished

--- aboutfashion/webapp/resources/views/pages/admin/editPromotion.blade.php
+++ aboutfashion/webapp/resources/views/pages/admin/editPromotion.blade.php
@@ -25,12 +25,16 @@
                         <!-- Discount -->
                         <label for="discount" class="form-label mt-4">Discount</label>
                         <input type="number" class="form-control" id="promotion_discount" value="{{$promotion->discount}}" name="discount">
+                        @php
+                            $formatStartDate = date('Y-m-d', strtotime($promotion->start_date));
+                            $formatFinalDate = date('Y-m-d', strtotime($promotion->final_date));
+                        @endphp
                         <!-- Start Date -->
                         <label for="start_date" class="form-label mt-4">Start Date</label>
-                        <input type="date" class="form-control" id="promotion_start_date" value="{{$promotion->start_date}}" name="start_date">
+                        <input type="date" class="form-control" id="promotion_start_date" value="{{$formatStartDate}}" name="start_date">
                         <!-- Final Date -->
                         <label for="final_date" class="form-label mt-4">Final Date</label>
-                        <input type="date" class="form-control" id="promotion_final_date" value="{{$promotion->final_date}}" name="final_date">
+                        <input type="date" class="form-control" id="promotion_final_date" value="{{$formatFinalDate}}" name="final_date">
                     </div>
                     <div class="modal-footer p-5 pe-0">
                         <span class="error-text me-auto" style="color:red"> </span>
